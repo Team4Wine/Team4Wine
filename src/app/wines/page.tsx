c10921d8d622951e89ddc1273be2880423510e5c
jiminp00 <115972184+jiminp00@users.noreply.github.com>
2024-11-29 15:54:54 +0900

    feat: /winelistpage v2 구현 (#12)
    * feat: 서치바 1차 구현
    
    * feat: 필터 1차 구현
    
    * feat: 서치바 2차 구현
    
    * feat: 필터 2차 구현(rating 기능)
    
    * fix: 변수 오류 수정
    
    * feat: 필터 3차 구현(price 기능)
    
    * fix: 체크박스 안보임 오류 해결
    
    * feat: 필터 리셋 핸들러 추가
    
    * fix: + 버튼 교체
    및 코드스타일 수정
    
    * fix: 미세수정

--- src/app/wines/page.tsx
+++ src/app/wines/page.tsx
@@ -1,8 +1,94 @@
+"use client";
+import { ChangeEvent, FormEvent, KeyboardEvent, useState } from "react";
 import styles from "./page.module.css";
 import RecommandSlide from "./RecommandSlide";
+import SearchBar from "./SearchBar";
 import SearchList from "./SearchList";
+import Filter from "./Filter";
+import { HiPlusCircle } from "react-icons/hi2";
+import { FilterState } from "./interfaces";
 
 export default function WineListPage() {
+  const [searchword, setSearchword] = useState<string>("");
+  const [filter, setFilter] = useState<FilterState>({
+    type: null,
+    minPrice: 0,
+    maxPrice: 100000,
+    rating: [false, false, false, false, false],
+  });
+
+  const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchword(e.target.value);
+  };
+
+  const handleSearchSubmit = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === "Enter") {
+      // searchword로 api get -> 리스트 업데이트
+      // 공백이 들어온 경우: 쿼리 없이 get
+    }
+  };
+
+  const handleFilterChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "type") {
+      // handleFilterTChange(e);
+    } else if (e.target.name === "maxPrice" || e.target.name === "minPrice") {
+      handleFilterPChange(e);
+    } else {
+      handleFilterRChange(e);
+    }
+  };
+
+  const handleFilterTChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFilter({
+      ...filter,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFilterPChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFilter({
+      ...filter,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFilterRChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const index = Number(e.target.name.slice(7, 8));
+    const ratingArray = filter.rating;
+    ratingArray[index] = e.target.checked;
+
+    setFilter({
+      ...filter,
+      rating: ratingArray,
+    });
+  };
+
+  const handleFilterSubmit = (e: FormEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    const target = e.target as HTMLButtonElement;
+
+    if (target.name == "apply") {
+      handleFilterApply();
+    } else if (target.name == "reset") {
+      handleFilterReset();
+    } else {
+      // console.log("handleFilterSubmit에서: 분기 오류");
+    }
+  };
+
+  const handleFilterReset = () => {
+    setFilter({
+      type: null,
+      minPrice: 0,
+      maxPrice: 100000,
+      rating: [false, false, false, false, false],
+    });
+  };
+
+  const handleFilterApply = () => {
+    // 제출 동작
+  };
+
   return (
     <div className={styles.page}>
       <div className={styles.header}>
@@ -14,19 +100,29 @@ export default function WineListPage() {
           <RecommandSlide />
         </div>
       </div>
-      <div className={styles.body}>
-        <div className={styles.search}>
-          <p>검색바</p>
+      <div className={styles.addContainer}>
+        <div className={styles.body}>
+          <div className={styles.search}>
+            <SearchBar
+              value={searchword}
+              onChange={handleSearchChange}
+              onKeyDown={handleSearchSubmit}
+            />
+          </div>
+          <div className={styles.filter}>
+            <Filter
+              value={filter}
+              onChange={handleFilterChange}
+              onSubmit={handleFilterSubmit}
+            />
+          </div>
+          <div className={styles.list}>
+            <SearchList />
+          </div>
         </div>
-        <div className={styles.filter}>
-          <p>필터</p>
+        <div className={styles.addObject}>
+          <HiPlusCircle className={styles.add} />
         </div>
-        <div className={styles.list}>
-          <SearchList />
-        </div>
-      </div>
-      <div className={styles.add}>
-        <p>+</p>
       </div>
     </div>
   );
