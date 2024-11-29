910f340de29db94c2a7448e46aa1679ea5790e9f
박지민 <25wlals@gmail.com>
2024-11-29 10:00:52 +0900

    fix: 와인카드 태그구조변경 및 리뷰오류 수정

--- src/app/wines/WineCard.tsx
+++ src/app/wines/WineCard.tsx
@@ -8,18 +8,9 @@ export default function WineCard(props: { data: WineData }) {
 
   const item: WineData = props.data;
 
-  let reviewOneLine: string;
-
-  if (item.recentReview) {
-    reviewOneLine = "테스트용 리뷰";
-    // console.log("---------");
-    // api 응답에 recentReview가 간략한 폼으로 들어가있음(schema와 실제응답 다름)
-
-    // reviewOneLine =
-    //   item.recentReview.aroma.join(", ") + " - " + item.recentReview.content;
-  } else {
-    reviewOneLine = "아직 리뷰가 없어요. 리뷰를 등록해 보세요!";
-  }
+  const reviewText = item.recentReview
+    ? item.recentReview.content
+    : "아직 리뷰가 없어요. 리뷰를 등록해 보세요!";
 
   return (
     <div className={styles.card}>
@@ -30,22 +21,16 @@ export default function WineCard(props: { data: WineData }) {
             src={item.image}
             alt="상품 이미지"
           />
-          <div className={styles.itemtext}>
-            <div className={styles.upperdiv}>
-              <div className={styles.infos}>
-                <p className={styles.name}>{item.name}</p>
-                <p className={styles.region}>{item.region}</p>
-              </div>
-              <div className={styles.reviews}>
-                <p className={styles.avgRating}>{item.avgRating}</p>
-                <StarRating grade={Math.round(item.avgRating)} />
-                <p className={styles.reviewCount}>
-                  {item.reviewCount}개의 후기
-                </p>
-              </div>
-            </div>
-            <div className={styles.lowerdiv}>
-              <p className={styles.price}>₩ {item.price}</p>
+          <div className={styles.infos}>
+            <p className={styles.name}>{item.name}</p>
+            <p className={styles.region}>{item.region}</p>
+            <p className={styles.price}>₩ {item.price}</p>
+          </div>
+          <div className={styles.reviews}>
+            <p className={styles.avgRating}>{item.avgRating}</p>
+            <StarRating grade={Math.round(item.avgRating)} />
+            <p className={styles.reviewCount}>{item.reviewCount}개의 후기</p>
+            <div className={styles.arrowcontainer}>
               <HiArrowRight className={styles.arrow} />
             </div>
           </div>
@@ -53,7 +38,7 @@ export default function WineCard(props: { data: WineData }) {
         <div className={styles.reviewpart}>
           <div className={styles.reviewdiv}>
             <p className={styles.reviewheader}>최신 후기</p>
-            <p className={styles.recentReview}>{reviewOneLine}</p>
+            <p className={styles.recentReview}>{reviewText}</p>
           </div>
         </div>
       </div>
